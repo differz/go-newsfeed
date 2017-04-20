3530611a94190c73a3012da4e6be6a5885f5bae1
Sergey Gychka <sergey.gychka@gmail.com>
2017-04-20 10:18:10 +0300

    add rss reader cli app

--- repository/article.go
+++ repository/article.go
@@ -2,12 +2,13 @@ package repository
 
 import (
 	"errors"
+	"fmt"
+	"log"
 	"time"
 
 	"github.com/VitaliiHurin/go-newsfeed/entity"
+	"upper.io/db.v3"
 	"upper.io/db.v3/lib/sqlbuilder"
-	"fmt"
-	"log"
 )
 
 type articleTable struct {
@@ -108,13 +109,13 @@ func (r *articleRepository) GetByTag(tid entity.TagID) ([]*entity.Article, error
 	return articles, nil
 }
 
-func (r *articleRepository) Store(a *entity.Article) error {
+func (r *articleRepository) Store(a *entity.Article) (*entity.Article, error) {
 	id, err := r.DB.Collection("article").Insert(newArticleTable(a))
 	if err != nil {
-		return err
+		return nil, err
 	}
 	a.ID = entity.ArticleID(id.(int64))
-	return nil
+	return a, nil
 }
 
 func (r *articleRepository) ChangeIsRead(aid entity.ArticleID, isRead entity.ArticleIsRead) error {
@@ -138,4 +139,54 @@ func (r *articleRepository) GetAll(){
 			time.Unix(birthday.DateCreated, 0).Format("January 2, 2006"),
 		)
 	}
+}
+
+func (r *articleRepository) FindById(id entity.ArticleID) (*entity.Article, error) {
+	var a *articleTable
+	err := r.DB.Collection("article").Find("id", id).One(&a)
+	if err != nil && err != db.ErrNoMoreRows{
+		return nil, err
+	}
+	if a != nil {
+		return assembleArticle(a), nil
+	}
+	return nil, errors.New("Not found")
+}
+
+func (r *articleRepository) AddTag(article *entity.Article, tag *entity.Tag) error {
+	at := &entity.ArticleTag{
+		ArticleID: article.ID,
+		TagID: tag.ID,
+	}
+	res := r.DB.Collection("article_tag_relation").Find(db.Cond{
+		"articleID": at.ArticleID,
+		"tagID":  at.TagID,
+	})
+	c, err := res.Count()
+	if err != nil {
+		return err
+	}
+	if c != 0 {
+		return nil
+	}
+	_, err = r.DB.Collection("article_tag_relation").Insert(newArticleTagTable(at))
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (r *articleRepository) FindByUrlAndSource(url string, sId entity.ServiceID) (*entity.Article, error) {
+	var a articleTable
+	err := r.DB.Collection("article").Find(db.Cond{
+		"url": url,
+		"serverID":  sId,
+	}).One(&a)
+	if err != nil {
+		if err == db.ErrNoMoreRows {
+			return nil, nil
+		}
+		return nil, err
+	}
+	return assembleArticle(&a), nil
 }
